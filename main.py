71b4337e17d4999b4f182d19f91cee816ffb9ceb
Dorian Kita <dorian.kita90@gmail.com>
2025-01-29 20:18:28 +0100

    created labels

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ canvas = Canvas(height=200, width=200)
 canvas.create_image(100,100, image=image)
 canvas.grid(row= 0, column= 1)
 
+website_label = Label(text="Website:")
+website_label.grid(row= 1, column= 0)
 
+email_label = Label(text="Email/Username:")
+email_label.grid(row= 2, column= 0)
+
+password_label = Label(text="Password:")
+password_label.grid(row= 3, column= 0)
 
 window.mainloop()
