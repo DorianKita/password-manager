82710810d7f135f81e08f50c88143b0636950f4e
Dorian Kita <dorian.kita90@gmail.com>
2025-01-29 21:30:30 +0100

    created save_data function to store data in separate file

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save_data():
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+
+    with open(file="data.txt", mode="a") as file:
+        file.write(f"{website} | {email} | {password}\n")
+
+    website_input.delete(0, END)
+    password_input.delete(0, END)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -30,9 +41,11 @@ password_label.grid(row= 3, column= 0)
 
 website_input = Entry(width= 35)
 website_input.grid(row= 1, column= 1, columnspan= 2)
+website_input.focus()
 
 email_input = Entry(width= 35)
 email_input.grid(row= 2, column = 1, columnspan = 2)
+email_input.insert(0, "example@example.com")
 
 password_input = Entry(width= 21)
 password_input.grid(row= 3, column= 1)
@@ -40,7 +53,7 @@ password_input.grid(row= 3, column= 1)
 generate_button = Button(text="Generate Password", width=10)
 generate_button.grid(row= 3, column= 2)
 
-add_button = Button(text="Add", width=33)
+add_button = Button(text="Add", width=33, command=save_data)
 add_button.grid(row= 4, column= 1, columnspan= 2)
 
 window.mainloop()
