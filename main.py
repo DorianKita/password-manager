35352b18f5da5cf1ab3d7dc06b4237fe1323d4e5
Dorian Kita <dorian.kita90@gmail.com>
2025-01-29 23:32:32 +0100

    added pyperclip to greater user experience

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 #Password Generator Project
@@ -35,6 +36,7 @@ def generate_password():
     password = "".join(password_list)
 
     password_input.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
